257416a57f6c2f8b9512707e75096baceebbac66
qazalnaderi <ghazalnaderi4@gmail.com>
2023-06-23 21:22:52 +0330

    neccassary methods added.

---
+++ Freccia/src/Main.java
@@ -0,0 +1,24 @@
+import  processing.core.PApplet;
+public class Main extends PApplet{
+    public static void main(String[] args) {
+        PApplet.main("Main",args);
+    }
+
+    @Override
+    public void setup(){
+    }
+
+    @Override
+    public void settings(){
+//        size(400,700);
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+
+
+
+}
